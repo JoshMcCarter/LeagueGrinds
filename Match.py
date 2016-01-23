f1e88a0f1103a283bb58e5affd7d7b33df5825b1
AndyGrant <Andrew@grantnet.us>
2016-01-22 22:25:36 -0500

    .

--- Match.py
+++ Match.py
@@ -4,18 +4,30 @@ class Match(object):
 		self.championID = data['champion']
 		self.matchID = data['matchId']
 		self.timestamp = data['timestamp']
+		self.playerID = playerID
 		
 		matchdata = riotWatcher.get_match(self.matchID);
+		for player in matchdata['participantIdentities']:
+			if player['player']['summonerId'] == playerID:
+				participantID = player['participantId']
+				break
+		else:
+			assert(False)
+				
+		self.kills   = matchdata['participants'][participantID-1]['stats']['kills']
+		self.assists = matchdata['participants'][participantID-1]['stats']['assists']
+		self.deaths  = matchdata['participants'][participantID-1]['stats']['deaths']		
 		
-		# Win / Loss
-		# Kills
-		# Assists
-		# Deaths
-		
-		for f in matchdata:
-			print f
-		assert(False)
-		
+		print self
 		
 	def __str__(self):
-		return self.data.__str__()
+		output = "========= Printing Match Object =========\n"
+		output+= "Player ID = " + str(self.playerID) +    "\n"
+		output+= "Match  ID = " + str(self.matchID)  +    "\n"
+		output+= "Champ  ID = " + str(self.championID) +  "\n"
+		output+= "TimeStamp = " + str(self.timestamp) +   "\n"
+		output+= "Kills     = " + str(self.kills) +       "\n"
+		output+= "Deaths    = " + str(self.deaths) +      "\n"
+		output+= "Assists   = " + str(self.assists) +     "\n"
+		output += "======== End Printing for Match ========\n"
+		return output
